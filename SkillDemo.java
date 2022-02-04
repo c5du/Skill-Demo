04f8c0a485a9b42a58020f15f02efdcc66070e64
Cat1029 <44483272+Cat1029@users.noreply.github.com>
2022-02-04 14:27:35 -0800

    fail

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,11 @@
+public class SkillDemo{
+    public static boolean even(int x){
+        if(x%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
 
